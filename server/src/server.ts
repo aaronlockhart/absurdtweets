c070287f5bff4126e1e11a99ab23c030860e378d
Aaron Lockhart <alockhart@spillman.com>
2017-04-23 21:54:40 -0600

    add boilerplate server code

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+
+export class Server {
+    public app: express.Application;
+
+    public static bootstrap(): Server {
+        return new Server();
+    }
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+        this.api();
+    }
+
+    public api() {
+
+    }
+
+    public config() {
+
+    }
+
+    public routes() {
+
+    }
+}
