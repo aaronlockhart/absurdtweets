115bd13fe129a822ce24afdf37ff880bccabffd3
Mayank Maheshwary <mmaheshwary@spillman.com>
2017-04-26 17:26:27 -0600

    Remove the @ symbol

--- client/src/app/mash-tweets/mash-tweets.component.ts
+++ client/src/app/mash-tweets/mash-tweets.component.ts
@@ -19,18 +19,21 @@ export class MashTweetsComponent implements OnInit {
 
     mash(): void {
 
+        let user1: string = '';
+        let user2: string = '';
+
         let atIndex: number = this.twitter_user1.indexOf('@');
         if(atIndex >= 0) {
-          this.twitter_user1.replace('@', '');
+          user1 = this.twitter_user1.replace('@', '');
         }
 
         atIndex = this.twitter_user2.indexOf('@');
         if(atIndex >= 0) {
-          this.twitter_user2.replace('@', '');
+          user2 = this.twitter_user2.replace('@', '');
         }
 
         // Call the function that takes the two handles and generates the mash
-        this.http.get('/api/mash/:'.concat(this.twitter_user1.concat(':'.concat(this.twitter_user2)))).subscribe((response) => {
+        this.http.get('/api/mash/:'.concat(user1.concat(':'.concat(user2)))).subscribe((response) => {
             this.tweet = response.text();
         });
     }
