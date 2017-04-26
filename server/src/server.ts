a7b29ccef83580b284dd95ef1970840018fc85e5
bonestormiii <aaron.t.lockhart@gmail.com>
2017-04-26 00:50:12 -0600

    added route to test twitter api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
+import { TwitterApp } from './twitter-app';
 
 export class Server {
     public app: express.Application;
+    private twitter: TwitterApp;
 
     public static bootstrap(): Server {
         return new Server();
@@ -9,6 +11,7 @@ export class Server {
 
     constructor() {
         this.app = express();
+        this.twitter = new TwitterApp();
         this.config();
         this.routes();
         this.api();
@@ -24,5 +27,15 @@ export class Server {
 
     public routes() {
         this.app.use(express.static('client'));
+        this.app.get('/test', this.routeTestHandler);
+    }
+
+    /**
+     * Handles requests to /test
+     */
+    private routeTestHandler(req: express.Request, res: express.Response) {
+        this.twitter.getLatestTweet('@realDonaldTrump').then((tweet) => {
+            res.send(tweet);
+        });
     }
 }
