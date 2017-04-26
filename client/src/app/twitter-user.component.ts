2bb1240cfae08b40999abe5ab76a2d94e2788ed5
Mayank Maheshwary <mmaheshwary@spillman.com>
2017-04-26 15:24:59 -0600

    Create initial page for entering the twitter handles

---
+++ client/src/app/twitter-user.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    templateUrl: './twitter-user.component.html',
+    styleUrls: ['./twitter-user.component.css']
+})
+
+export class twitterUser {
+
+    twitter_user1_name: string;
+    twitter_user2_name: string;
+
+}
