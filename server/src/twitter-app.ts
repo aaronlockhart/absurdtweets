5b4da5287b3631ca265eb589d1d5ec15332b9fbc
Mayank Maheshwary <mmaheshwary@spillman.com>
2017-04-26 12:28:45 -0600

    Fixed the promise

--- server/src/twitter-app.ts
+++ server/src/twitter-app.ts
@@ -20,26 +20,24 @@ export class TwitterApp {
 
     // We can only retrieve the latest 3200 tweets from the timeline
     getLatestTweetsByCount(username: string, count: string): Promise<string[]> {
-        return this.twitter.getUserTimeline({screen_name: username, count: count}, this.error, this.success)
-               .toPromise()
-               .then(response => response as string[]);
-    }
 
-    error(error, response, body): void {
-        console.log('ERROR [%s]', error);
-    }
-
-    success = (data) => {
+        return new Promise<string[]>((resolve, reject) => {
+            this.twitter.getUserTimeline({screen_name: username, count: count},
+            (error, response, body) => reject(error),
+            (data) => {
         
-        let tweets: string[] = new Array();
-        let i: number;
-        let obj = JSON.parse(data) as [any];
+                let tweets: string[] = new Array();
+                let i: number;
+                let obj = JSON.parse(data) as [any];
 
-        for(let item of obj) {
-            let tweet = this.removeLinks(item.text);
-            tweets.push(tweet);
-            console.log('Tweet [%s]', tweet);
-        }
+                for(let item of obj) {
+                    let tweet = this.removeLinks(item.text);
+                    tweets.push(tweet);
+                    console.log('Tweet [%s]', tweet);
+                }
+                resolve(tweets);
+            })
+        });
     }
 
     removeLinks(tweet: string): string {
