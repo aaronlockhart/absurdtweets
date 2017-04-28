58c67411c8fd61853a8166bbb956b0a82facd112
Mayank Maheshwary <mmaheshwary@spillman.com>
2017-04-27 18:42:04 -0600

    Sentences are sentence-cased now.

--- server/src/ngram-random-sentence.ts
+++ server/src/ngram-random-sentence.ts
@@ -14,7 +14,7 @@ export class NGramRandomSentence {
     public getRandomSentence(desiredLength: number): string {
 
         let generatedSentence: string = snm.generateSentence(this.model, desiredLength);
-
+        let finalSentence: string = '';
         // Find the last punctuation (., ! or ?)
         let lastIndexOfPeriod = generatedSentence.lastIndexOf('.');
         let lastIndexOfExclamation = generatedSentence.lastIndexOf('!');
@@ -22,6 +22,44 @@ export class NGramRandomSentence {
 
         let lastIndexOfPunctuation = Math.max(lastIndexOfPeriod, lastIndexOfExclamation, lastIndexOfQuestion);
 
-        return generatedSentence.substring(0, lastIndexOfPunctuation+1);
+        //return generatedSentence.substring(0, lastIndexOfPunctuation+1);
+        generatedSentence = generatedSentence.substring(0, lastIndexOfPunctuation+1);
+
+        // Now capitalize every sentence
+        let indexOfPunctuation: number = 0;
+        let tempSubStr: string = '';
+        let isFirstSentence: boolean = true;
+        let charAtIndex: number = 0;
+        let tempFinalSentence: string = '';
+        // Before we start, lets save the final sentence to be the generated sentence, in case there is no punctuation found
+        finalSentence = generatedSentence;
+
+        while(true) {
+            indexOfPunctuation = generatedSentence.search('[.!?]');
+            if(indexOfPunctuation == -1) {
+                break;
+            }
+            tempSubStr = generatedSentence.substring(0, indexOfPunctuation+1);
+
+            if(isFirstSentence) {
+                charAtIndex = 0;
+                isFirstSentence = false;
+            }
+            else {
+                charAtIndex = 1;
+            }
+
+            let firstChar = tempSubStr.substring(0,charAtIndex+1);
+            firstChar = firstChar.toUpperCase();
+            tempSubStr = firstChar.concat(tempSubStr.substring(charAtIndex+1));
+            tempFinalSentence = tempFinalSentence.concat(tempSubStr);
+            generatedSentence = generatedSentence.substring(indexOfPunctuation+1);
+        }
+
+        if(tempFinalSentence != '') {
+            finalSentence = tempFinalSentence;
+        }
+
+        return finalSentence;
     }
 }
